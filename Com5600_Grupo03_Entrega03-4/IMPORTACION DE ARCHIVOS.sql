afc6bd0a4168600c695a291f99560b516c44fb1b
maurixoda123 <mauricio.rodriguez19@hotmail.com>
2024-11-29 14:08:39 -0300

    Update IMPORTACION DE ARCHIVOS.sql

--- Com5600_Grupo03_Entrega03-4/IMPORTACION DE ARCHIVOS.sql
+++ Com5600_Grupo03_Entrega03-4/IMPORTACION DE ARCHIVOS.sql
@@ -5,9 +5,9 @@
 --NOMBRE DE LA MATERIA: BASE DE DATOS APLICADA
 --NUMERO DEL GRUPO: 03
 --INTEGRANTES: 
---			MODESTTI, TOM¡S AGUSTÕN (45073572)
+--			MODESTTI, TOM√ÅS AGUST√çN (45073572)
 --			NIEVAS, VALENTIN LISANDRO (45464487)
---			QUI—ONEZ, LUCIANO FEDERICO (45007142)
+--			QUI√ëONEZ, LUCIANO FEDERICO (45007142)
 --			RODRIGUEZ, MAURICIO EZEQUIEL (42774942)
 -------------------------------------------------------------------
 USE Com5600G03
@@ -25,41 +25,41 @@ GO
 CREATE OR ALTER PROCEDURE Info.importarSucursal (@RutaArchivo NVARCHAR(MAX), @NombreHoja NVARCHAR(50))
 AS 
 BEGIN
- BEGIN TRY
-			DECLARE @Consulta nvarchar(MAX)
-			-- Creamos la tabla temporal 
-			CREATE TABLE #SucursalTemporal
-			(
-				ciudad varchar (15),
-				reemplazadaX  varchar(20) not null check (reemplazadaX  IN ('San Justo','Ramos Mejia','Lomas del Mirador')),
-				direccion varchar(150),
-				horario varchar(50),
-				telefono varchar(9) check (telefono like '5555-555[0-9]')
-			) 
-			-- Generamos la consulta para importar el archivo
-			SET @Consulta = N'
-				INSERT INTO #SucursalTemporal(ciudad,reemplazadaX,direccion,horario,telefono)
-				SELECT *
-				FROM OPENROWSET(
-				''Microsoft.ACE.OLEDB.12.0'',
-				''Excel 12.0; Database=' + @RutaArchivo + '; HDR=YES;'',
-				''SELECT * FROM ['+@nombreHoja+']'');'
-			--Ejecutamos la consulta para importar el archivo a la tabla temporal 
-			EXEC sp_executesql @Consulta 
-			SELECT * FROM #SucursalTemporal
-			--Insertamos en nuestra tabla la informacion ue contiene la tabla temporal 
-			INSERT INTO Info.Sucursal(ciudad,reemplazadaX,direccion,horario,telefono)
-			SELECT * FROM #SucursalTemporal st
-			WHERE NOT EXISTS (SELECT 1 FROM Info.Sucursal s WHERE s.ciudad=st.ciudad and s.direccion=st.direccion) -- Funciona para que las sucursales que figuran en la tabla no se dupliquen 
-
-			--Elimamos la tabla temporal 
-			PRINT 'Los datos se insertaron exitosamente' 
-			DROP TABLE #SucursalTemporal
- END TRY 
- BEGIN CATCH 
-  PRINT 'No se pudo importar la informacion de las sucursales ' + @RutaArchivo 
-  PRINT ERROR_MESSAGE() 
- END CATCH 
+ 	BEGIN TRY
+		DECLARE @Consulta nvarchar(MAX)
+		-- Creamos la tabla temporal 
+		CREATE TABLE #SucursalTemporal
+		(
+			ciudad varchar (15),
+			reemplazadaX  varchar(20) not null check (reemplazadaX  IN ('San Justo','Ramos Mejia','Lomas del Mirador')),
+			direccion varchar(150),
+			horario varchar(50),
+			telefono varchar(9) check (telefono like '5555-555[0-9]')
+		) 
+		-- Generamos la consulta para importar el archivo
+		SET @Consulta = N'
+			INSERT INTO #SucursalTemporal(ciudad,reemplazadaX,direccion,horario,telefono)
+			SELECT *
+			FROM OPENROWSET(
+			''Microsoft.ACE.OLEDB.12.0'',
+			''Excel 12.0; Database=' + @RutaArchivo + '; HDR=YES;'',
+			''SELECT * FROM ['+@nombreHoja+']'');'
+		--Ejecutamos la consulta para importar el archivo a la tabla temporal 
+		EXEC sp_executesql @Consulta 
+		SELECT * FROM #SucursalTemporal
+		--Insertamos en nuestra tabla la informacion ue contiene la tabla temporal 
+		INSERT INTO Info.Sucursal(ciudad,reemplazadaX,direccion,horario,telefono)
+		SELECT * FROM #SucursalTemporal st
+		WHERE NOT EXISTS (SELECT 1 FROM Info.Sucursal s WHERE s.ciudad=st.ciudad and s.direccion=st.direccion) -- Funciona para que las sucursales que figuran en la tabla no se dupliquen 
+	
+		--Elimamos la tabla temporal 
+		PRINT 'Los datos se insertaron exitosamente' 
+		DROP TABLE #SucursalTemporal
+ 	END TRY 
+ 	BEGIN CATCH 
+	  	PRINT 'No se pudo importar la informacion de las sucursales ' + @RutaArchivo 
+	  	PRINT ERROR_MESSAGE() 
+ 	END CATCH 
 END 
 GO
 
@@ -69,49 +69,49 @@ GO
 CREATE OR ALTER PROCEDURE Info.importarEmpleados (@RutaArchivo NVARCHAR(MAX), @NombreHoja NVARCHAR(50))
 AS 
 BEGIN
- BEGIN TRY
-			DECLARE @Consulta nvarchar(MAX)
-			--Creamos la tabla temporal 
-			CREATE TABLE #EmpleadosTemporal
-			(
-				idEmpleado int,
-				nombre nvarchar (255),
-				apellido nvarchar(255),
-				dni int,
-				direccion varchar(255),
-				emailPesonal nvarchar(255),
-				emailEmpresa nvarchar(255),
-				cargo varchar(60) check (cargo IN ('Cajero','Supervisor','Gerente de sucursal')),
-				sucursal varchar(60),
-				turno varchar(25)
-			) 
-			--Generamos la consulta para importar el archivo 
-			SET @Consulta = N'
-				INSERT INTO #EmpleadosTemporal(idEmpleado,nombre,apellido,dni,direccion,emailPesonal,emailEmpresa,cargo,sucursal,turno)
-				SELECT [Legajo/ID],Nombre,Apellido,DNI,Direccion,[email personal],[email empresa],Cargo,Sucursal,Turno
-				FROM OPENROWSET(
-				''Microsoft.ACE.OLEDB.12.0'',
-				''Excel 12.0; Database=' + @RutaArchivo + '; HDR=YES;'',
-				''SELECT * FROM ['+@nombreHoja+']'');'
-			--Ejecutamos la consulta 
-			EXEC sp_executesql @Consulta 
+ 	BEGIN TRY
+		DECLARE @Consulta nvarchar(MAX)
+		--Creamos la tabla temporal 
+		CREATE TABLE #EmpleadosTemporal
+		(
+			idEmpleado int,
+			nombre nvarchar (255),
+			apellido nvarchar(255),
+			dni int,
+			direccion varchar(255),
+			emailPesonal nvarchar(255),
+			emailEmpresa nvarchar(255),
+			cargo varchar(60) check (cargo IN ('Cajero','Supervisor','Gerente de sucursal')),
+			sucursal varchar(60),
+			turno varchar(25)
+		) 
+		--Generamos la consulta para importar el archivo 
+		SET @Consulta = N'
+			INSERT INTO #EmpleadosTemporal(idEmpleado,nombre,apellido,dni,direccion,emailPesonal,emailEmpresa,cargo,sucursal,turno)
+			SELECT [Legajo/ID],Nombre,Apellido,DNI,Direccion,[email personal],[email empresa],Cargo,Sucursal,Turno
+			FROM OPENROWSET(
+			''Microsoft.ACE.OLEDB.12.0'',
+			''Excel 12.0; Database=' + @RutaArchivo + '; HDR=YES;'',
+			''SELECT * FROM ['+@nombreHoja+']'');'
+		--Ejecutamos la consulta 
+		EXEC sp_executesql @Consulta 
 
-			SELECT * FROM #EmpleadosTemporal
-			--Insertamos en nuestra tabla la informacion que contiene la tabla temporal 
-			INSERT INTO Info.Empleado(nombre,apellido,dni,direccion,emailPesonal,emailEmpresa,cargo,sucursal,turno,idSucursal)
-			SELECT nombre,apellido,dni,direccion,emailPesonal,emailEmpresa,cargo,sucursal,turno,
-			 (SELECT idSucursal FROM Info.Sucursal suc WHERE suc.reemplazadaX = empTemp.sucursal) as idSucursal
-			 FROM #EmpleadosTemporal empTemp
-			 WHERE NOT EXISTS (SELECT 1 FROM Info.Empleado e WHERE empTemp.dni= e.dni) AND empTemp.idEmpleado IS NOT NULL --Evitamos duplicados y basura del archivo 
+		SELECT * FROM #EmpleadosTemporal
+		--Insertamos en nuestra tabla la informacion que contiene la tabla temporal 
+		INSERT INTO Info.Empleado(nombre,apellido,dni,direccion,emailPesonal,emailEmpresa,cargo,sucursal,turno,idSucursal)
+		SELECT nombre,apellido,dni,direccion,emailPesonal,emailEmpresa,cargo,sucursal,turno,
+		 (SELECT idSucursal FROM Info.Sucursal suc WHERE suc.reemplazadaX = empTemp.sucursal) as idSucursal
+		 FROM #EmpleadosTemporal empTemp
+		 WHERE NOT EXISTS (SELECT 1 FROM Info.Empleado e WHERE empTemp.dni= e.dni) AND empTemp.idEmpleado IS NOT NULL --Evitamos duplicados y basura del archivo 
 
-			--Eliminamos la tabla temporal 
-			PRINT 'Los datos se insertaron exitosamente' 
-			DROP TABLE #EmpleadosTemporal		
- END TRY 
- BEGIN CATCH 
-  PRINT 'No se pudieron importar los empleados ' + @RutaArchivo 
-  PRINT ERROR_MESSAGE() 
- END CATCH 
+		--Eliminamos la tabla temporal 
+		PRINT 'Los datos se insertaron exitosamente' 
+		DROP TABLE #EmpleadosTemporal		
+ 	END TRY 
+ 	BEGIN CATCH 
+  		PRINT 'No se pudieron importar los empleados ' + @RutaArchivo 
+  		PRINT ERROR_MESSAGE() 
+ 	END CATCH 
 END 
 GO
 -------------------------------------------------------------------------------------------------------------
@@ -121,39 +121,39 @@ CREATE OR ALTER PROCEDURE Prod.importarClasificacionProductos (@RutaArchivo NVAR
 AS 
 BEGIN
  BEGIN TRY
-			DECLARE @Consulta nvarchar(MAX)
-			--Creamos la tabla temporal 
-			CREATE TABLE #ClasificacionTemporal
-			(
-				lineaProducto varchar(20),
-				producto varchar(50)
-			) 
-			--Generamos la consulta con SQL Dinamico para importar el archivo a la tabla temporal 
-			SET @Consulta = N'
-				INSERT INTO #ClasificacionTemporal(lineaProducto,producto)
-				SELECT *
-				FROM OPENROWSET(
-				''Microsoft.ACE.OLEDB.12.0'',
-				''Excel 12.0; Database=' + @RutaArchivo + '; HDR=YES;'', --Especifica que es un archivo Excel, la ruta del archivo, que hay encabezado y la hoja en la que se encuentran los datos
-				''SELECT * FROM ['+@nombreHoja+']'');'
-			--Ejecutamos la consulta
-			EXEC sp_executesql @Consulta 
+		DECLARE @Consulta nvarchar(MAX)
+		--Creamos la tabla temporal 
+		CREATE TABLE #ClasificacionTemporal
+		(
+			lineaProducto varchar(20),
+			producto varchar(50)
+		) 
+		--Generamos la consulta con SQL Dinamico para importar el archivo a la tabla temporal 
+		SET @Consulta = N'
+			INSERT INTO #ClasificacionTemporal(lineaProducto,producto)
+			SELECT *
+			FROM OPENROWSET(
+			''Microsoft.ACE.OLEDB.12.0'',
+			''Excel 12.0; Database=' + @RutaArchivo + '; HDR=YES;'', --Especifica que es un archivo Excel, la ruta del archivo, que hay encabezado y la hoja en la que se encuentran los datos
+			''SELECT * FROM ['+@nombreHoja+']'');'
+		--Ejecutamos la consulta
+		EXEC sp_executesql @Consulta 
 
-			SELECT * FROM #ClasificacionTemporal
-			--Inserto en la tabla de clasificacion la informacion que se encuentra en la tabla temporal
-			INSERT INTO Prod.Clasificacion(lineaProducto,producto)
-			SELECT lineaProducto,producto FROM #ClasificacionTemporal ct
-			WHERE NOT EXISTS (SELECT 1 FROM Prod.Clasificacion c WHERE c.lineaProducto=ct.lineaProducto AND c.producto=ct.producto)
+		SELECT * FROM #ClasificacionTemporal
+		--Inserto en la tabla de clasificacion la informacion que se encuentra en la tabla temporal
+		INSERT INTO Prod.Clasificacion(lineaProducto,producto)
+		SELECT lineaProducto,producto FROM #ClasificacionTemporal ct
+		WHERE NOT EXISTS (SELECT 1 FROM Prod.Clasificacion c WHERE c.lineaProducto=ct.lineaProducto AND c.producto=ct.producto)
 
-			--Elimino la tabla temporal
-			PRINT 'Los datos se insertaron exitosamente' 
-			DROP TABLE #ClasificacionTemporal
- END TRY 
- BEGIN CATCH 
-  --En caso de fallar la hora de la importacion mostraria el mensaje con el error correspondiente 
-  PRINT 'No se pudieron importar la clasificacion de productos' + @RutaArchivo 
-  PRINT ERROR_MESSAGE() 
- END CATCH 
+		--Elimino la tabla temporal
+		PRINT 'Los datos se insertaron exitosamente' 
+		DROP TABLE #ClasificacionTemporal
+ 	END TRY 
+ 	BEGIN CATCH 
+		--En caso de fallar la hora de la importacion mostraria el mensaje con el error correspondiente 
+		PRINT 'No se pudieron importar la clasificacion de productos' + @RutaArchivo 
+		PRINT ERROR_MESSAGE() 
+ 	END CATCH 
 END 
 GO
 
@@ -163,7 +163,7 @@ GO
 CREATE OR ALTER PROCEDURE Prod.importarCatalogo (@RutaArchivo nvarchar(MAX))
 AS 
 BEGIN 
-			-- Creamos la tabla temporal
+		-- Creamos la tabla temporal
             CREATE TABLE #CatalogoTemporal 
             (
 				id int,
@@ -209,38 +209,38 @@ GO
 CREATE OR ALTER PROCEDURE Prod.importarProductosElectronicos (@RutaArchivo NVARCHAR(MAX),@NombreHoja NVARCHAR(50))
 AS 
 BEGIN  
-			DECLARE @Consulta nvarchar(MAX)
-			DECLARE @Categoria varchar(50) ='Electronica'
-			
-			--Creamos la tabla temporal donde vamos a descargar nuestro archivo
-			CREATE TABLE #ElectronicoTemporal
-			(
-				Product varchar(100),
-				Precio_Unitario_en_dolares decimal(10,2)
-			)
+		DECLARE @Consulta nvarchar(MAX)
+		DECLARE @Categoria varchar(50) ='Electronica'
+		
+		--Creamos la tabla temporal donde vamos a descargar nuestro archivo
+		CREATE TABLE #ElectronicoTemporal
+		(
+			Product varchar(100),
+			Precio_Unitario_en_dolares decimal(10,2)
+		)
 
-			--Generamos la consulta de importacion 
-			SET @Consulta = N'
-				INSERT INTO #ElectronicoTemporal(Product,Precio_Unitario_en_dolares)
-					SELECT Product, [Precio Unitario en dolares]
-						FROM OPENROWSET(
-								''Microsoft.ACE.OLEDB.12.0'',
-								''Excel 12.0; Database=' + @RutaArchivo + '; HDR=YES;'', --Especifica que es un archivo Excel, la ruta del archivo, que hay encabezado y la hoja en la que se encuentran los datos
-								''SELECT * FROM ['+@NombreHoja+']'');'
-			--Ejecutamos la consulta
-			EXEC sp_executesql @Consulta;
+		--Generamos la consulta de importacion 
+		SET @Consulta = N'
+			INSERT INTO #ElectronicoTemporal(Product,Precio_Unitario_en_dolares)
+				SELECT Product, [Precio Unitario en dolares]
+					FROM OPENROWSET(
+							''Microsoft.ACE.OLEDB.12.0'',
+							''Excel 12.0; Database=' + @RutaArchivo + '; HDR=YES;'', --Especifica que es un archivo Excel, la ruta del archivo, que hay encabezado y la hoja en la que se encuentran los datos
+							''SELECT * FROM ['+@NombreHoja+']'');'
+		--Ejecutamos la consulta
+		EXEC sp_executesql @Consulta;
 
-			--Muestro la tabla temporal para verificar que la informacion se descargo exitosamente 
-			SELECT * FROM #ElectronicoTemporal;
-	
-			--Pasamos la informacion a nuestra tabla
-			INSERT INTO Prod.Catalogo(categoria,nombreProducto,precioUnidad,fecha)
-				SELECT @Categoria,Product,Precio_Unitario_en_dolares,GETDATE() FROM #ElectronicoTemporal
-			WHERE NOT EXISTS (SELECT 1 FROM Prod.Catalogo WHERE nombreProducto=Product)
+		--Muestro la tabla temporal para verificar que la informacion se descargo exitosamente 
+		SELECT * FROM #ElectronicoTemporal;
 
-			--Eliminamos la tabla temporal 
-			PRINT 'Los datos se insertaron exitosamente' 
-			DROP TABLE #ElectronicoTemporal
+		--Pasamos la informacion a nuestra tabla
+		INSERT INTO Prod.Catalogo(categoria,nombreProducto,precioUnidad,fecha)
+			SELECT @Categoria,Product,Precio_Unitario_en_dolares,GETDATE() FROM #ElectronicoTemporal
+		WHERE NOT EXISTS (SELECT 1 FROM Prod.Catalogo WHERE nombreProducto=Product)
+
+		--Eliminamos la tabla temporal 
+		PRINT 'Los datos se insertaron exitosamente' 
+		DROP TABLE #ElectronicoTemporal
 END
 
 EXECUTE Prod.importarProductosElectronicos  @RutaArchivo = 'C:\Program Files\Microsoft SQL Server\MSSQL16.SQLEXPRESS01\TRABAJO PRACTICO\TP_integrador_Archivos\Productos\Electronic accessories.xlsx', @nombreHoja ='Sheet1$' 
@@ -251,37 +251,37 @@ GO
 CREATE OR ALTER PROCEDURE Prod.importarProductosImportados (@RutaArchivo nvarchar(MAX), @NombreHoja nvarchar(50))
 AS 
 BEGIN
-			-- Declaramos la variable consulta y la tabla temporal 
-			DECLARE @Consulta NVARCHAR(MAX) 
-			CREATE TABLE #ImportadosTemporal
-			(
-				IdProducto int,
-				NombreProducto varchar(100),
-				Proveedor varchar(100),
-				CategorÌa varchar(50),
-				CantidadPorUnidad varchar(255),
-				PrecioUnidad decimal(10,2)
-			) 
-			-- Generamos la consulta con SQL Dinamico para importar el archivo a la tabla temporal
-			SET @Consulta = N'
-				INSERT INTO #ImportadosTemporal(IdProducto,NombreProducto,Proveedor,CategorÌa,CantidadPorUnidad,PrecioUnidad)
-				SELECT IdProducto,NombreProducto,Proveedor,CategorÌa,CantidadPorUnidad,PrecioUnidad
-				FROM OPENROWSET(
-					''Microsoft.ACE.OLEDB.12.0'', --Proveedor OLEB
-					''Excel 12.0; Database=' + @RutaArchivo + '; HDR=YES;'', --Especifica que es un archivo Excel, la ruta del archivo, que hay encabezado y la hoja en la que se encuentran los datos
-					''SELECT * FROM ['+@nombreHoja+']'');'  
-			-- Ejecutamos la consulta
-			EXEC sp_executesql @Consulta 
+		-- Declaramos la variable consulta y la tabla temporal 
+		DECLARE @Consulta NVARCHAR(MAX) 
+		CREATE TABLE #ImportadosTemporal
+		(
+			IdProducto int,
+			NombreProducto varchar(100),
+			Proveedor varchar(100),
+			Categor√≠a varchar(50),
+			CantidadPorUnidad varchar(255),
+			PrecioUnidad decimal(10,2)
+		) 
+		-- Generamos la consulta con SQL Dinamico para importar el archivo a la tabla temporal
+		SET @Consulta = N'
+			INSERT INTO #ImportadosTemporal(IdProducto,NombreProducto,Proveedor,Categor√≠a,CantidadPorUnidad,PrecioUnidad)
+			SELECT IdProducto,NombreProducto,Proveedor,Categor√≠a,CantidadPorUnidad,PrecioUnidad
+			FROM OPENROWSET(
+				''Microsoft.ACE.OLEDB.12.0'', --Proveedor OLEB
+				''Excel 12.0; Database=' + @RutaArchivo + '; HDR=YES;'', --Especifica que es un archivo Excel, la ruta del archivo, que hay encabezado y la hoja en la que se encuentran los datos
+				''SELECT * FROM ['+@nombreHoja+']'');'  
+		-- Ejecutamos la consulta
+		EXEC sp_executesql @Consulta 
 
-			-- Verificamos si los datos se importaron finalmente a la tala temporal
-			SELECT * FROM #ImportadosTemporal
-			-- Insertamos a nuestra tabla, las filas que se encuentran en la tabla temporal
-			INSERT INTO Prod.Catalogo(nombreProducto,categoria,precioUnidad,fecha)
-			SELECT NombreProducto,CategorÌa,PrecioUnidad,GETDATE() FROM #ImportadosTemporal it
-			WHERE NOT EXISTS (SELECT 1 FROM Prod.Catalogo WHERE nombreProducto= it.NombreProducto)
-			-- Eliminamos la tabla temporal 
-			PRINT 'Los datos se insertaron exitosamente' 
-			DROP TABLE #ImportadosTemporal
+		-- Verificamos si los datos se importaron finalmente a la tala temporal
+		SELECT * FROM #ImportadosTemporal
+		-- Insertamos a nuestra tabla, las filas que se encuentran en la tabla temporal
+		INSERT INTO Prod.Catalogo(nombreProducto,categoria,precioUnidad,fecha)
+		SELECT NombreProducto,Categor√≠a,PrecioUnidad,GETDATE() FROM #ImportadosTemporal it
+		WHERE NOT EXISTS (SELECT 1 FROM Prod.Catalogo WHERE nombreProducto= it.NombreProducto)
+		-- Eliminamos la tabla temporal 
+		PRINT 'Los datos se insertaron exitosamente' 
+		DROP TABLE #ImportadosTemporal
 END 
 GO
 
@@ -292,12 +292,11 @@ CREATE OR ALTER PROCEDURE Ven.importarVentas (@RutaArchivo varchar(MAX))
 AS
 BEGIN
     BEGIN TRY
-
         DECLARE @Consulta nvarchar(max);
         DECLARE @IVA decimal(10,2)= 0.21
 		DECLARE @ESTADO varchar(50) = 'Pagada'
 
-        -- DefiniciÛn de la tabla temporal #VentasRegistradas
+        -- Definici√≥n de la tabla temporal #VentasRegistradas
         DROP TABLE IF EXISTS #VentasRegistradas;
         CREATE TABLE #VentasRegistradas
         (
@@ -367,7 +366,7 @@ BEGIN
         INNER JOIN Ven.Venta ve ON ve.Fecha = v.fecha AND ve.Hora = v.hora AND ve.Id_Empleado = v.idEmpleado
 		WHERE NOT EXISTS (SELECT 1 FROM Ven.Detalle_Venta WHERE Numero_factura= v.idFactura)
 
-        -- Eliminamos la tabla temporal ya que no la necesitaremos la informaciÛn almacenada allÌ
+        -- Eliminamos la tabla temporal ya que no la necesitaremos la informaci√≥n almacenada all√≠
         DROP TABLE #VentasRegistradas;
 
     END TRY
